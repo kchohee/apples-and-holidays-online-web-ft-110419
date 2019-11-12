afd0eea201a4c3a4522fe45ff7f3e5c07ffb970c
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-12 23:11:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  retun holiday_supplies[:summer][:fourth_of_july][2]
+  retun holiday_hash[:summer][:fourth_of_july][2]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)

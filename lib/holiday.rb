fe202d32adc99b596c37d0dcb84413d28cf329ef
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-12 23:35:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season] = holiday_name
-  holiday[season][holiday] = supply_array
+  holiday[season][holiday] << supply_array
   return add_new_holiday_with_supplies
 
 end

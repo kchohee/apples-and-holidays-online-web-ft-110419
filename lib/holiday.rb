025636c8854b60b64a9593d3d6dac306fbd4ce4a
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 01:18:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,5 +77,6 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists include the string "BBQ"
 holiday_arr = []
 holiday_hash.each {|season,holiday| holiday_arr << holiday  }
+binding.pry
 end
 all_holidays_with_bbq(holiday_supplies)

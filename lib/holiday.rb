0f9eafd61f269ca26faf0de9edb4e7f1131082f2
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 00:57:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do | season, holiday|
    puts "#{season.to_s.capitalize}:"
    holiday.each do |holi, supply|
-   puts "  #{holi.to_s.capitalize.split("_").join(" ")}: #{supply.join(", ")}"
+   puts "  #{holi.to_s.split("_").join(" ").capitalize}: #{supply.join(", ")}"
 # binding.pry
   end
  end

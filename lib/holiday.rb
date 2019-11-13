035037b8835e87bd228b8764d5a1c76407b8d0df
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 00:02:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,8 +34,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
- winter_needs = []
- holiday_hash[:winter].each {|k,v| winter_needs << v }
+ holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)

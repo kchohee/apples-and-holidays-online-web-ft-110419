e3c5afe4f9847a3f6b97be743d6dbc198e083db7
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 00:08:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
- holiday_hash.each do | season, holiday, supp|
+ holiday_hash.each do | season, holiday, sup|
    puts "#{season.capitalize}:"
    puts "#{holiday.capitalize}: #{sup}"
 end
